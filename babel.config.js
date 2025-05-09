f1507279d0d0bf7c50ba240a2d2b7f0ab391d74f
ArfaatHussain <arfaathussain745@gmail.com>
2025-05-09 20:16:43 +0500

    Configured fully

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'], // 👈 Add this line
-  };
-  
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'react-native-reanimated/plugin',
+      {
+        worklets: false, // Disable worklets if not needed
+      },
+    ],
+    [
+      '@babel/plugin-transform-private-methods',
+      { loose: true }, // Ensure 'loose' mode is consistent
+    ],
+    [
+      '@babel/plugin-transform-class-properties',
+      { loose: true }, // Add this plugin with consistent 'loose' setting
+    ],
+    [
+      '@babel/plugin-transform-private-property-in-object',
+      { loose: true }, // Add this plugin with consistent 'loose' setting
+    ],
+  ],
+};
